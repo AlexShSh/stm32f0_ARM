df4288256da0a0357b4ae06524ed6fed98cb3aac
Alexey Shashin <shashin.aeh@phystech.edu>
2019-04-19 20:56:05 +0300

    time on button

---
+++ mytasks/time_button/main.c
@@ -0,0 +1,225 @@
+/*
+ * This example demonstrates using timers to capture input
+ * signal
+ */
+
+#include "stm32f0xx_ll_rcc.h"
+#include "stm32f0xx_ll_system.h"
+#include "stm32f0xx_ll_bus.h"
+#include "stm32f0xx_ll_gpio.h"
+#include "stm32f0xx_ll_tim.h"
+#include "stm32f0xx_ll_utils.h"
+#include "stm32f0xx_ll_cortex.h"
+
+/**
+  * System Clock Configuration
+  * The system Clock is configured as follow :
+  *    System Clock source            = PLL (HSI/2)
+  *    SYSCLK(Hz)                     = 48000000
+  *    HCLK(Hz)                       = 48000000
+  *    AHB Prescaler                  = 1
+  *    APB1 Prescaler                 = 1
+  *    HSI Frequency(Hz)              = 8000000
+  *    PLLMUL                         = 12
+  *    Flash Latency(WS)              = 1
+  */
+static void rcc_config()
+{
+    /* Set FLASH latency */
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+
+    /* Enable HSI and wait for activation*/
+    LL_RCC_HSI_Enable();
+    while (LL_RCC_HSI_IsReady() != 1);
+
+    /* Main PLL configuration and activation */
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
+                                LL_RCC_PLL_MUL_12);
+
+    LL_RCC_PLL_Enable();
+    while (LL_RCC_PLL_IsReady() != 1);
+
+    /* Sysclk activation on the main PLL */
+    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+
+    /* Set APB1 prescaler */
+    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+    /* Update CMSIS variable (which can be updated also
+     * through SystemCoreClockUpdate function) */
+    SystemCoreClock = 48000000;
+}
+
+/*
+ * Clock on GPIOC and set two led pins
+ */
+static void gpio_config(void)
+{
+	/*
+     * Init port for indicator
+     */
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_5, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_6, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_7, LL_GPIO_MODE_OUTPUT);
+
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
+    return;
+}
+
+/*
+ * Configure timer to input capture mode
+ */
+static void timers_config(void)
+{
+    /*
+     * Configure input channel
+     */
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+    LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_5, LL_GPIO_MODE_ALTERNATE);
+    LL_GPIO_SetAFPin_0_7(GPIOA, LL_GPIO_PIN_5, LL_GPIO_AF_2);
+    LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_5, LL_GPIO_PULL_UP);
+
+    /*
+     * Setup timer to capture input mode
+     */
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+    LL_TIM_SetPrescaler(TIM2, 47999);
+    LL_TIM_IC_SetFilter(TIM2, LL_TIM_CHANNEL_CH1, LL_TIM_IC_FILTER_FDIV16_N5);
+    LL_TIM_IC_SetPolarity(TIM2, LL_TIM_CHANNEL_CH1,
+                          LL_TIM_IC_POLARITY_FALLING);
+    LL_TIM_IC_SetActiveInput(TIM2, LL_TIM_CHANNEL_CH1,
+                             LL_TIM_ACTIVEINPUT_DIRECTTI);
+    LL_TIM_IC_SetPrescaler(TIM2, LL_TIM_CHANNEL_CH1, LL_TIM_ICPSC_DIV1);
+    LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
+    LL_TIM_EnableIT_CC1(TIM2);
+    LL_TIM_EnableCounter(TIM2);
+    /*
+     * Setup NVIC
+     */
+    NVIC_EnableIRQ(TIM2_IRQn);
+    NVIC_SetPriority(TIM2_IRQn, 1);
+    
+    LL_TIM_GenerateEvent_UPDATE(TIM2);
+    return;
+}
+
+static void set_dyn_indicator(uint16_t number)
+{
+    static int digit_num = 0;
+
+    static uint32_t mask = LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | \
+                           LL_GPIO_PIN_3 | LL_GPIO_PIN_4 | LL_GPIO_PIN_5 | \
+                           LL_GPIO_PIN_6 | LL_GPIO_PIN_7;
+
+    static const uint32_t decoder[] = {
+        LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 | \
+        LL_GPIO_PIN_4 | LL_GPIO_PIN_5, // 0
+        LL_GPIO_PIN_1 | LL_GPIO_PIN_2, // 1
+        LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_6 | LL_GPIO_PIN_4 | \
+        LL_GPIO_PIN_3, // 2
+        LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_6 | LL_GPIO_PIN_2 | \
+        LL_GPIO_PIN_3, // 3
+        LL_GPIO_PIN_5 | LL_GPIO_PIN_6 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2, // 4
+        LL_GPIO_PIN_0 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 | LL_GPIO_PIN_5 | \
+        LL_GPIO_PIN_6, // 5
+        LL_GPIO_PIN_0 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 | LL_GPIO_PIN_4 | \
+        LL_GPIO_PIN_5 | LL_GPIO_PIN_6, //6
+        LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2, //7
+        LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 | \
+        LL_GPIO_PIN_4 | LL_GPIO_PIN_5 | LL_GPIO_PIN_6, //8
+        LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 | \
+        LL_GPIO_PIN_5 | LL_GPIO_PIN_6 //9
+    };
+
+    uint32_t port_state = LL_GPIO_ReadOutputPort(GPIOB);
+
+    static uint32_t digit_mask = LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3;
+    LL_GPIO_WriteOutputPort(GPIOC, digit_mask);
+    
+    switch(digit_num)
+    {
+        case 0:
+            number = (number / 1000) % 10;
+            LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0);
+            port_state = (port_state & ~mask) | decoder[number];
+            break;
+        case 1:
+            number = (number / 100) % 10;
+            LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_1);
+            port_state = (port_state & ~mask) | decoder[number];
+            break;
+        case 2:
+            number = (number / 10) % 10;
+            LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_2);
+            port_state = (port_state & ~mask) | decoder[number] | LL_GPIO_PIN_7;
+            break;
+        case 3:
+            number %= 10;
+            LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_3);
+            port_state = (port_state & ~mask) | decoder[number];
+            break;
+        default:
+            break;
+    }
+    LL_GPIO_WriteOutputPort(GPIOB, port_state);
+
+    digit_num = (digit_num + 1) % 4;
+    return;
+}
+
+static uint32_t out_num = 0;
+
+void TIM2_IRQHandler(void)
+{
+    static uint32_t old = 0;
+    uint32_t cur = LL_TIM_IC_GetCaptureCH1(TIM2);
+    
+    if (old == 0)
+    {
+        old = cur;
+    }
+    else
+    {
+        out_num = cur - old;
+        old = cur;
+    }
+
+    LL_TIM_ClearFlag_CC1(TIM2);
+}
+
+static void systick_config(void)
+{
+    LL_InitTick(48000000, 1000);
+    LL_SYSTICK_EnableIT();
+    NVIC_SetPriority(SysTick_IRQn, 0);
+    return;
+}
+
+void SysTick_Handler(void)
+{
+    set_dyn_indicator(out_num / 100);
+}
+
+
+int main(void)
+{
+    rcc_config();
+    gpio_config();
+    timers_config();
+    systick_config();
+
+    while (1);
+    return 0;
+}
